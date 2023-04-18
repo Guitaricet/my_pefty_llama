88b484c81e76515930d6a89495157d02546c83a0
Jason Phang <jasonphang@nyu.edu>
2023-04-18 00:04:15 -0400

    ia3 implementation

---
+++ pefty_llama/peft/__init__.py
@@ -0,0 +1,2 @@
+from .configuration import *
+from .ia3 import IA3ForAttn, IA3ForMLP
---
+++ pefty_llama/peft/configuration.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+PEFT_PREFIX = "prefix"
+PEFT_PREFIX_ADAPTER = "prefix_adapter"
+PEFT_PROMPT = "prefix_adapter"
+PEFT_HOULSBY_ADAPTER = "houlsby_adapter"
+PEFT_PFEIFFER_ADAPTER = "pfeiffer_adapter"
+PEFT_IA3 = "ia3"
+NO_PEFT = "nothing"
+
+
+@dataclass
+class PeftConfig:
+    peft_mode: str
+
+    # Used by prompt, prefix, prefix_adapter
+    num_prefix_tokens: int = None
+
+    # Prefix
+    prefix_use_mlp: bool = True
+
+    # LoRA
+    lora_rank: int = 8
+    lora_alpha: int = 16
+
+    # IA3
+
+    def check(self):
+        assert self.peft_mode in (
+            PEFT_PREFIX, PEFT_PREFIX_ADAPTER, PEFT_PROMPT, PEFT_HOULSBY_ADAPTER, PEFT_PFEIFFER_ADAPTER,
+            PEFT_IA3,
+            NO_PEFT,
+        )
